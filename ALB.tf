fa0cce044f7d9dbee44a2a4ceebaecc84deddbf9
George Alta <george.alta.nz@gmail.com>
2025-05-26 23:12:53 +1200

    ALB adjusted for HTTP

--- ALB.tf
+++ ALB.tf
@@ -8,15 +8,15 @@ resource "aws_lb" "web_alb" {
 
 resource "aws_lb_target_group" "web_tg" {
   name     = "web-tg"
-  port     = 443
-  protocol = "HTTPS"
+  port     = 80
+  protocol = "HTTP"
   vpc_id   = aws_vpc.wp_vpc.id
 }
 
 resource "aws_lb_target_group_attachment" "web_attach" {
   target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = aws_instance.wordpress.id
-  port             = 443
+  port             = 80
 }
 
 resource "aws_lb_listener" "https_listener" {
@@ -36,23 +36,23 @@ resource "aws_lb_listener" "https_listener" {
 }
 
 # An HTTP listener (port 80) that redirects to HTTPS
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "http_listener" {
+#   load_balancer_arn = aws_lb.web_alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
-    redirect {
-      host        = "#{host}"
-      path       = "/#{path}"
-      port       = "443"
-      protocol   = "HTTPS"
-      query      = "#{query}"
-      status_code = "HTTP_301"
-    }
-  }
-}
+#   default_action {
+#     type = "redirect"
+#     redirect {
+#       host        = "#{host}"
+#       path       = "/#{path}"
+#       port       = "443"
+#       protocol   = "HTTPS"
+#       query      = "#{query}"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
 
 # make the load balancer point to the EC2 instance in EC2_wordpress.tf
 
